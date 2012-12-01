321150b0eb2e2a2475480be1320f9cb0dc24c826
sumito3478 <sumito3478@gmail.com>
2012-12-02 07:04:56 +0900

    Remove trailing whitespace in Build.scala

--- project/Build.scala
+++ project/Build.scala
@@ -5,16 +5,16 @@ import com.twitter.sbt._
 object SumitoTextBuild extends Build {
 
   lazy val sumitoText = Project(
-    id = "sumito-text", 
-    base = file("."), 
+    id = "sumito-text",
+    base = file("."),
     settings = StandardProject.newSettings
   ).settings(
     Seq(
-      version := "0.0.1", 
-      scalaVersion := "2.10.0-RC3", 
+      version := "0.0.1",
+      scalaVersion := "2.10.0-RC3",
       libraryDependencies ++= Seq(
-        "org.specs2" %% "specs2" % "1.12.3" % "test", 
-        "org.mockito" % "mockito-core" % "1.9.5" % "test", 
+        "org.specs2" %% "specs2" % "1.12.3" % "test",
+        "org.mockito" % "mockito-core" % "1.9.5" % "test",
         "junit" % "junit" % "4.11" % "test",
         "com.ibm.icu" % "icu4j" % "50.1"
       )

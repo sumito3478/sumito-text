1ef756c907ed116c9f3344f4a0c73b174ff630d6
sumito3478 <sumito3478@gmail.com>
2012-12-03 07:18:05 +0900

    Add IteratorView.apply

--- src/main/scala/sumito3478/collection/IteratorView.scala
+++ src/main/scala/sumito3478/collection/IteratorView.scala
@@ -7,4 +7,15 @@ trait IteratorView[+A] extends Iterator[A] {
 }
 
 object IteratorView {
+  def apply[A, B, It <: Iterator[A]](it: It)(f: (It) => B): IteratorView[B] = {
+    new IteratorView[B] {
+      def hasNext: Boolean = {
+        it.hasNext
+      }
+
+      def next: B = {
+        f(it)
+      }
+    }
+  }
 }

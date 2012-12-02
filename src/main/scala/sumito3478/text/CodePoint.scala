63ee7763386ec27bdaed16e4c4b31765e54ba44d
sumito3478 <sumito3478@gmail.com>
2012-12-03 07:11:33 +0900

    Add CodePoint#toChars

--- src/main/scala/sumito3478/text/CodePoint.scala
+++ src/main/scala/sumito3478/text/CodePoint.scala
@@ -3,7 +3,9 @@ package sumito3478.text
 import scala.collection.mutable.{ Seq => MutableSeq }
 
 class CodePoint(val value: Int) extends AnyVal {
-
+  def toChars: IndexedSeq[Char] = {
+    Character.toChars(value)
+  }
 }
 
 object CodePoint {

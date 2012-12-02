80cde62741529860f8a0e9564e06694a8d3482ae
sumito3478 <sumito3478@gmail.com>
2012-12-03 07:16:21 +0900

    Add sumito3478.collection.immutable.ThreadLocal

---
+++ src/main/scala/sumito3478/collection/immutable/ThreadLocal.scala
@@ -0,0 +1,11 @@
+package sumito3478.collection.immutable
+
+import java.lang.{ ThreadLocal => JThreadLocal }
+
+trait ThreadLocal[+A] extends sumito3478.collection.ThraedLocal[A] {
+
+}
+
+object ThreadLocal {
+}
+

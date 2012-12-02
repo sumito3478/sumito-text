516a52daccb7f982c0873fe431cacee4ad56884e
sumito3478 <sumito3478@gmail.com>
2012-12-03 06:58:18 +0900

    Add sumito3478.text.collection.IteratorText

---
+++ src/main/scala/sumito3478/text/collection/IteratorText.scala
@@ -0,0 +1,24 @@
+package sumito3478.text.collection
+
+import scala.collection.Iterator
+
+import com.ibm.icu.text.BreakIterator
+import com.ibm.icu.util.ULocale
+
+import IteratorText.DefaultGraphemeBreakIterator
+import sumito3478.collection.IteratorView
+import sumito3478.collection.immutable.ThreadLocal
+import sumito3478.text.CodePoint
+import sumito3478.text.Grapheme
+import sumito3478.text.Line
+import sumito3478.text.Sentence
+import sumito3478.text.Word
+import sumito3478.text.collection.immutable.StringText
+
+trait IteratorText extends Iterator[Char] {
+
+}
+
+object IteratorText {
+
+}

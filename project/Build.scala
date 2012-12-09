da50080e3ea3623687c08e034b984461e73fa7b6
sumito3478 <sumito3478@gmail.com>
2012-12-10 02:38:12 +0900

    Import project settings improv. from sumito-base

--- project/Build.scala
+++ project/Build.scala
@@ -12,6 +12,30 @@ object SumitoTextBuild extends Build {
     Seq(
       version := "0.0.1",
       scalaVersion := "2.10.0-RC3",
+      scalacOptions ++= Seq(
+        "-target:jvm-1.7",
+        "-deprecation",
+        "-feature",
+        "-explaintypes",
+        "-unchecked",
+        "-optimise"
+        ),
+      publishMavenStyle := true,
+      publishTo := Some(Resolver.sftp(
+        "sumito3478 Maven Repository",
+        "maven.sumito3478.info",
+        46877,
+        "/var/www/maven.sumito3478.info") as (
+        "sumito3478",
+        new java.io.File(
+          new java.io.File(System.getProperty("user.home")),
+          ".ssh/sumito3478-sshkey"))),
+      resolvers := Seq(
+        "Maven Repository Mirror" at "http://uk.maven.org/maven2"),
+      externalResolvers <<= resolvers map {
+        rs =>
+          Resolver.withDefaultResolvers(rs, mavenCentral = false)
+      },
       libraryDependencies ++= Seq(
         "org.specs2" %% "specs2" % "1.12.3" % "test",
         "org.mockito" % "mockito-core" % "1.9.5" % "test",

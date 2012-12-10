b1ae46d6f95819ef9341ab0fe410df8e3d21bafe
sumito3478 <sumito3478@gmail.com>
2012-12-10 09:53:03 +0900

    Add commons-lang to libraryDependencies

--- project/Build.scala
+++ project/Build.scala
@@ -38,6 +38,7 @@ object Build extends Build {
           Resolver.withDefaultResolvers(rs, mavenCentral = false)
       },
       libraryDependencies ++= Seq(
+        "commons-lang" % "commons-lang" % "2.6",
         "org.specs2" %% "specs2" % "1.12.3" % "test",
         "org.mockito" % "mockito-core" % "1.9.5" % "test",
         "junit" % "junit" % "4.11" % "test",
